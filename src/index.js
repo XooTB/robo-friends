7e1ca4cb984bc25e1251b50ee50483127c3f07df
thalfpant <xo.ot2001@gmail.com>
2021-12-19 12:06:33 +0600

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -7,9 +7,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// <Card name={robots[0].name} id={robots[0].id} email={robots[0].email}
